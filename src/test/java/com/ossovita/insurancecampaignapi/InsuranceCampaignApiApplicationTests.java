bb271569e828d825a7cf67e3fde00c5257f8b616
yasineryigit <yasineryigit737@gmail.com>
2023-04-27 17:17:30 +0300

    Initial commit

---
+++ src/test/java/com/ossovita/insurancecampaignapi/InsuranceCampaignApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossovita.insurancecampaignapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCampaignApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
